b73cddf542b865f6f4d0148cc12a97f1c93a697b
Felipe Rodrigues <felipe@felipevr.com>
2020-05-06 15:35:52 -0300

    Add poetry install on post-gen

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -4,14 +4,28 @@ after a project is generated
 """
 import os
 
+
 def init_git():
+    """
+    Initializes a git repo in the newly created folder
+    """
     os.system("git init")
 
-def install_pre_commit():
-    os.system("pre-commit install --install-hooks ")
-    os.system("pre-commit install -t pre-push ")
 
+def install_pre_commit():
+    """
+    Installs the git hooks through Pre-Commit
+    """
+    os.system("poetry run pre-commit install --install-hooks ")
+    os.system("poetry run pre-commit install -t pre-push ")
+
+def instal_poetry_dependencies():
+    """
+    Installs all the dependencies defined in poetry
+    """
+    os.system("poetry install")
 
 if __name__ == "__main__":
     init_git()
+    instal_poetry_dependencies()
     install_pre_commit()
