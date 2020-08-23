15a20b7561cdbe73c4631a738d1b4c2824ffee11
Felipe Rodrigues <felipe@felipevr.com>
2020-08-23 10:33:42 -0300

    Add poetry-vscode python intepreter path
    Now VSCode will _automagically_ know where to find the Python
    interpreter managed by Poetry

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -2,6 +2,10 @@
 Commands that are executed by Cookiecutter
 after a project is generated
 """
+import logging
+import json
+import pathlib
+import subprocess
 import os
 
 
@@ -19,13 +23,44 @@ def install_pre_commit():
     os.system("pre-commit install --install-hooks")
     os.system("pre-commit install -t pre-push")
 
+
 def instal_poetry_dependencies():
     """
     Installs all the dependencies defined in poetry
     """
     os.system("poetry install")
 
+
+def set_vscode_python_path():
+    """
+    Sets VSCode's `python.pythonPath` to the
+    current interpreter managed by Poetry
+    """
+    result = subprocess.run(
+        ["poetry", "run", "which", "python"], stdout=subprocess.PIPE, check=True
+    )
+    python_path = result.stdout.decode("utf-8")[:-1]
+
+    settings_path = pathlib.Path(".vscode/settings.json")
+
+    if not settings_path.is_file():
+        logging.warning(".vscode/settings.json not found. Skipping pythonPath setting.")
+        return
+
+    settings = json.load(open(settings_path))
+
+    if "python.pythonPath" in settings:
+        logging.warning(
+            "There already is a 'pythonPath' entry. Skipping pythonPath setting."
+        )
+
+    settings["python.pythonPath"] = python_path
+
+    json.dump(settings, open(settings_path, 'wt'), indent=4, sort_keys=True)
+
+
 if __name__ == "__main__":
     init_git()
     instal_poetry_dependencies()
     install_pre_commit()
+    set_vscode_python_path()
